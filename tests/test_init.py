dfe2f741b4c7671a316c79506725ac51262001c9
co <corentin.bel@protonmail.com>
2024-04-30 12:05:55 +0200

    name modif

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,10 +1,10 @@
 import whisper_timestamped
-
+from trigger_gen import Parser
 
 def test_init1():
     assert help(whisper_timestamped) != None
 
 def test_first_book():
-    parser = whisper_timestamped.Parser()
+    parser = Parser()
     words = parser.parse_audio("hello.wav")
     assert words.head(1).text == "I"
